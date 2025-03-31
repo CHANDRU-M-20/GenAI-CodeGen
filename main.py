be71154198f1e6e7fe415823e3e2050d1417ac8d
Chandru M <csechandrucse@gmail.com>
2025-03-31 22:29:47 +0530

    add : ui

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from model import LLM
+import streamlit as st
+
+
+st.header("Google Gemini LLM")
+st.write("This is a simple Streamlit app to interact with Google Gemini LLM.")
 
 genai_model = LLM("gemini-1.5-pro")
 print("LLM initialized successfully.")
 llm = genai_model.get_llm()
-response = llm.invoke("What is the capital of France?")
-print(response.content)
+
+input = st.text_input("Enter your query:", key="query_input") 
+
+if st.button("Submit"):
+    response = llm.invoke(input)
+
+    st.write("Response from LLM:")
+    st.write(response.content)
+    print(response.content)
 
